55f3dafc69b60cbbdc04cb2b5f9503f7f5a041d6
Aleksandr <nord112009@bk.ru>
2022-06-25 18:40:29 +0300

    add task 38

---
+++ Task38/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. 
+  Найдите разницу между максимальным и минимальным элементов массива.
+  [3 7 22 2 78] -> 76
+*/
+
+int[] array = new int[5];
+
+
+
+FillArrayRandomNumbers(array);
+PrintArray(array);
+SumOddNumbers(array);
+
+void SumOddNumbers(int[] array)
+{
+int max = array[0];
+int min = array[0]; 
+int difference = 0; 
+for(int i = 0; i < array.Length; i ++)
+   {
+       if(max < array[i])
+       {
+         max = array[i];
+       }
+       if(min > array[i])
+       {
+         min = array[i];
+       }
+   }
+difference = max - min;
+
+Console.WriteLine($"{max}-{min}={difference}");
+}
+
+void PrintArray(int[] array)
+{
+   for(int i = 0; i < array.Length; i++)
+   {
+      Console.Write(array[i] + " ");
+   }
+Console.WriteLine();
+}
+
+void FillArrayRandomNumbers(int[] array)
+{
+   for(int i = 0; i < array.Length; i++)
+   {
+      array[i] = new Random().Next(0,100);
+   }
+}
